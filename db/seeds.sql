1f9bc8e185d05022f7de65fa5859cf05f0372d64
LAPTOP\Diana <dviann89@gmail.com>
2018-11-21 16:39:22 -0600

    Connection and server.js set-up

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ("Cheese burger");
+INSERT INTO burgers (burger_name) VALUES ("Baconator");
+INSERT INTO burgers (burger_name) VALUES ("Guac");
+INSERT INTO burgers (burger_name) VALUES ("Double");
+
