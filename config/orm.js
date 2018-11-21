734e8e679dd18b6274cb5c00e6bd767c7963bfce
LAPTOP\Diana <dviann89@gmail.com>
2018-11-21 17:43:37 -0600

    ORM file done

---
+++ config/orm.js
@@ -0,0 +1,94 @@
+//Import MySQL connection
+const connection = require("../config/connection.js");
+
+function printQuestionMarks(num) {
+    let arr = [];
+
+    for (let i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
+//Converts object key, value pair to SQL syntax
+function objToSql(ob) {
+    let arr = [];
+
+    for (let key in ob) {
+        let value = ob[key];
+
+        if (Object.hasOWnproperty.call(ob, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            arr.push(key + "=" + value);
+        }
+    }
+
+    return arr.toString();
+}
+
+const orm = {
+    all: function (tableInput, cb) {
+        let queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    create: function (table, cols, vals, cb) {
+        let queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+    update: function (table, objColVals, condition, cb) {
+        let queryString = "UPDATE" + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString); 
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    delete: function(table, condition, cb) {
+        let queryString = "DELETE FROM " + table;
+        queryString +- "WHERE ";
+        queryString +- condition;
+
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        }); 
+    }
+};
+
+//Export the orm to burger.js
+module.exports = orm;
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,2 +1,58 @@
 //Express routes 
 
+const express = require("express");
+
+const routes = express.Router();
+
+const burger = ("../models/burger.js");
+
+router.get("/", function(req, res) {
+    call.all(function(data){
+        let hbsObject = {
+            burger: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("api/burgers", function(req, res){
+    burger.create([
+        "burger_name", "devoured"
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], function(result) {
+        res.json({id: result.insertId});
+    });
+});
+
+router.put("/api/burger/:id", function(req, res){
+    let condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function(result) {
+        if (result.changeRows === 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+router.delete("/api/burger/:id", function(req, res){
+    let condition = "id = " + req.param.id;
+
+    burger.delete(condition, function(result){
+        if (result.affectedRows === 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+//Exports routes for server.js
+module.exports = router;
