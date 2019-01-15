e38f9afba3e2616f7720e3199df888436e102ee2
LamarC <dviann89@gmail.com>
2019-01-14 19:17:35 -0600

    Server error fixed

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,8 +7,8 @@ const router = express.Router();
 const burger = ("../models/burger.js");
 console.log(Object.keys(burger))
 
-router.get("/", function(req, res) {
-    burger.selectAll(function(data) {
+router.get("/", function (req, res) {
+    burger.all(function (data) {
         let hbsObject = {
             burger: data
         };
@@ -17,24 +17,24 @@ router.get("/", function(req, res) {
     });
 });
 
-router.post("api/burgers", function(req, res){
-    burger.insertOne([
+router.post("api/burgers", function (req, res) {
+    burger.create([
         "burger_name"
     ], [
         req.body.burger_name
-    ], function(data) {
+    ], function (data) {
         res.redirect('/');
     });
 });
 
-router.put("/api/burger/:id", function(req, res){
+router.put("/api/burger/:id", function (req, res) {
     let condition = "id = " + req.params.id;
 
     console.log("condition", condition);
 
-    burger.updateOne({
+    burger.update({
         devoured: req.body.devoured
-    }, condition, function(data) {
+    }, condition, function (data) {
         res.redirect('/');
     });
 });
--- server.js
+++ server.js
@@ -2,14 +2,17 @@ const express = require("express");
 
 const methodOverride = require('method-override');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
 app.use(express.static("public"));
 
-app.use(express.urlencoded({ extended:
-false }));
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use(express.json());
 
 app.use(methodOverride("_method"));
 
@@ -27,6 +30,6 @@ const routes = require("./controllers/burgers_controller.js");
 app.use("/", routes);
 
 //Start server so it can listen
-app.listen(PORT, function(){
+app.listen(PORT, function () {
     console.log("Server listening on: http://localhost:" + PORT);
 });
